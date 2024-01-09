fc355e5034e23883422f705358a89d27ad2c66eb
Jonoth1 <jonoth123@gmx.de>
2024-01-09 10:16:28 +0100

    Schleifen für Halo Exchange

--- src/heat_update.cpp
+++ src/heat_update.cpp
@@ -19,8 +19,8 @@ void start_halo_exchange(Field *temperature, ParallelData *parallel)
     int width = temperature->ny + 2;
 
     // (up <-> down)
-    i = 1;
-    for( j = 1, j <= temperature->ny, j++){
+    j = 1;
+    for( i = 1; i <= temperature->nx; i++){
         send_buffer_up[j - 1] = temperature->data[idx(i,j,width)];
     }
     // Communication 1: Send data to the upper neighbor and receive from the lower neighbor
@@ -29,14 +29,26 @@ void start_halo_exchange(Field *temperature, ParallelData *parallel)
     MPI_Irecv(recv_buffer_down, temperature->nx, MPI_DOUBLE, parallel->ndown, ROW_TAG_UP,parallel->comm, MPI_NULL, &(parallel->requests[1]));
     
     // (down <-> up)
+    j = temperature->ny;
+    for( i = 1; i <= temperature->nx; i++){
+        send_buffer_up[j - 1] = temperature->data[idx(i,j,width)];
+    }
     // Communication 2: Send data to the lower neighbor and receive from the upper neighbor
     // This exchanges the ghost cells in the bottom row of the local temperature field
 
     // (left <-> right)
+    i = 1;
+    for( j = 1; j <= temperature->nx; j++){
+        send_buffer_up[j - 1] = temperature->data[idx(i,j,width)];
+    }
     // Communication 3: Send data to the left neighbor and receive from the right neighbor
     // This exchanges the ghost cells in the leftmost column of the local temperature field
 
     // (right <-> left)
+    i = temperature->ny;
+    for( j = 1; j <= temperature->nx; j++){
+        send_buffer_up[j - 1] = temperature->data[idx(i,j,width)];
+    }
     // Communication 4: Send data to the right neighbor and receive from the left neighbor
     // This exchanges the ghost cells in the rightmost column of the local temperature field
 }
@@ -109,11 +121,14 @@ void update_boundary_temperature(Field *curr, Field *prev, double a, double dt, 
     int width;
     width = curr->ny + 2;
     double dx2, dy2;
-
+    double x_const, y_const;
+            
     //  Determine the temperature field at the next time step. 
     //  As fixed boundary conditions are applied, the outermost grid points are not updated.
-    dx2 = prev->dx * prev->dx;
+    dx2 = prev->dx * prev->dx; 
     dy2 = prev->dy * prev->dy;
+    x_const = (a*dt/ (dx2));
+    y_const = (a*dt/ (dx2));
 
     /*Unterschied zu update_interior_temperature ist, dass fuer die verschiedenen Grenzen, 
     die Konstantenwerte aus init_heat_field in heat_init.cpp genutzt werden*/
@@ -132,8 +147,8 @@ void update_boundary_temperature(Field *curr, Field *prev, double a, double dt, 
             // Apply the five-point stencil to update the temperature at the left borders.
 
             curr->data[ic] = prev->data[ic] 
-                + (x_const * (prev->data[iu] - (2 * prev->data[ic]) + prev->data[id])) 
-                + (y_const * (prev->data[ir] - (2 * prev->data[ic]) + 20.0));
+            + (x_const * (prev->data[iu] - (2 * prev->data[ic]) + prev->data[id])) 
+            + (y_const * (prev->data[ir] - (2 * prev->data[ic]) + prev->data[il]));
         }
     }
 
@@ -151,27 +166,8 @@ void update_boundary_temperature(Field *curr, Field *prev, double a, double dt, 
             // Apply the five-point stencil to update the temperature at the right borders.
 
             curr->data[ic] = prev->data[ic] 
-                + (x_const * (prev->data[iu] - (2 * prev->data[ic]) + prev->data[id])) 
-                + (y_const * (70.0 - (2 * prev->data[ic]) + prev->data[il]));
-        }
-    }
-
-    // Update the lower border
-    if (coords[0] == dims[0] - 1) {
-        j = curr->ny;
-        for (i = 1; i < curr->nx + 1; i++)
-        {
-            ic = idx(i, j, width);
-            iu = idx(i + 1, j, width);
-            id = idx(i - 1, j, width);
-            ir = idx(i, j + 1, width);
-            il = idx(i, j - 1, width);
-
-            // Apply the five-point stencil to update the temperature at the lower borders.
-
-            curr->data[ic] = prev->data[ic] 
-                + (x_const * (prev->data[iu] - (2 * prev->data[ic]) + 5.0)) 
-                + (y_const * (prev->data[ir] - (2 * prev->data[ic]) + prev->data[il]));
+            + (x_const * (prev->data[iu] - (2 * prev->data[ic]) + prev->data[id])) 
+            + (y_const * (prev->data[ir] - (2 * prev->data[ic]) + prev->data[il]));
         }
     }
 
@@ -189,8 +185,27 @@ void update_boundary_temperature(Field *curr, Field *prev, double a, double dt, 
             // Apply the five-point stencil to update the temperature at the upper boundary.
 
             curr->data[ic] = prev->data[ic] 
-                + (x_const * (85.0 - (2 * prev->data[ic]) + prev->data[id])) 
-                + (y_const * (prev->data[ir] - (2 * prev->data[ic]) + prev->data[il]));
+            + (x_const * (prev->data[iu] - (2 * prev->data[ic]) + prev->data[id])) 
+            + (y_const * (prev->data[ir] - (2 * prev->data[ic]) + prev->data[il]));
+        }
+    }
+    
+    // Update the lower border
+    if (coords[0] == dims[0] - 1) {
+        j = curr->ny;
+        for (i = 1; i < curr->nx + 1; i++)
+        {
+            ic = idx(i, j, width);
+            iu = idx(i + 1, j, width);
+            id = idx(i - 1, j, width);
+            ir = idx(i, j + 1, width);
+            il = idx(i, j - 1, width);
+
+            // Apply the five-point stencil to update the temperature at the lower borders.
+
+            curr->data[ic] = prev->data[ic] 
+            + (x_const * (prev->data[iu] - (2 * prev->data[ic]) + prev->data[id])) 
+            + (y_const * (prev->data[ir] - (2 * prev->data[ic]) + prev->data[il]));
         }
     }
 }
